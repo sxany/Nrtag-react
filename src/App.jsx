94e7fcf4da0b0cde60ff93ea556247e3afb06edc
sxany <97228203+sxany@users.noreply.github.com>
2025-01-27 15:56:59 +0800

    Penyederhanaan Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@
 import React from 'react';
 import Navbar from './navbar';
 import Jumbotron from './Jumbotron';
-
 import './assets/css/input.css'
+import Carousel from './Carousel';
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Jumbotron/>
-   
+      <Jumbotron/>         
     </div>
   );
 };
--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,8 +4,12 @@ import './assets/css/input.css';
 
 const Carousel = () => {
     return (
-         
+         /* Carousel */        
+         <div>
+         <p className='relative left-10 font-title'>Peran Game Dalam Membuat Lingkungan Nyaman</p>
+         <p1 className='relative left-10 font-title text-[10px] text-[#706969]' >12 jam lalu </p1>
+       </div>
     );
 };
 
-export default ;
+export default Carousel ;
