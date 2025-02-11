f148608bbe69cd887cd0ebd2e806d366bfdca8fa
sxany <97228203+sxany@users.noreply.github.com>
2025-02-11 12:28:56 +0800

    adding fixed on iframe

--- src/Gameplay.jsx
+++ src/Gameplay.jsx
@@ -8,12 +8,12 @@ const Gameplay = () => {
             {/*kotak*/}
             <div className='relative z-10 flex space-x-2 ml-6'>
                 <div className='kotak w-[34px] h-[13px] mt-4 ml-7'></div>
-                    <p className=' text-black mt-1.5 opacity-100 grid' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '500', fontStyle: 'normal', fontSize:'20px'}}>Random Gameplay</p>
+                    <p className=' text-black mt-1.5 opacity-100 ' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '500', fontStyle: 'normal', fontSize:'20px'}}>Musik Hari Ini</p>
                     
                     {/*Youtube iframe*/}
                         </div>
                         <div>
-                        <iframe className='z-10 ml-24 opacity-100' width="300" height="200" src="https://www.youtube.com/embed/YX40ZDUIJT8?si=mJXin0u6HugBgQUn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <iframe className='fixed ml-24 rounded-md' width="300" height="200" src="https://www.youtube.com/embed/?listType=playlist&list=RDdJf4wCdLU18&autoplay=1&mute=1"  title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullScreen></iframe>
                         </div>
                                                        
                                     
