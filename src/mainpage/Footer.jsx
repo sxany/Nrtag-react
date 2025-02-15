790711d96ee6a3ad13d92338f5ec936c3ff85c88
sxany <97228203+sxany@users.noreply.github.com>
2025-02-15 15:41:07 +0800

    mx-auto footer

--- src/mainpage/Footer.jsx
+++ src/mainpage/Footer.jsx
@@ -4,8 +4,8 @@ import '../assets/css/input.css';
 
 const Footer = () => {
     return (
-        <footer className='absolute justify-center justify-items-center mt-5' style={{backgroundColor:'#000',width:'100%', height:'auto' }}>
-            <h style={{color:'#ffff', fontFamily: "Familjen Grotesk, serif", fontWeight: '500', fontStyle: 'normal', fontSize: '36px'}}>Nrtag Developer</h>
+        <footer className='mx-auto justify-items-center mt-5' style={{backgroundColor:'#2B2B2B',width:'1280px', height:'auto', margin:'' }}>
+            <h1 style={{color:'#ffff', fontFamily: "Familjen Grotesk, serif", fontWeight: '500', fontStyle: 'normal', fontSize: '36px'}}>Nrtag Developer</h1>
             <p className='mt-1' style={{color:'#ffff', fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize: '18px', maxWidth:'500px', textAlign:'center', lineHeight:'1.8'}}> We are committed to providing users with the latest gaming news delivering up-to-date trends releases and industry insights. </p>
         </footer>
     );
