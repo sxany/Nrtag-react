f0d0e6bf6ed160af75d6f9241fe80a1d01192e46
sxany <97228203+sxany@users.noreply.github.com>
2025-02-19 13:43:48 +0800

    menambahkan file news

---
+++ src/content/News.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/css/local.css';
+import '../assets/css/input.css';
+
+const News = () => {
+    return (
+        <div>
+            <h1>Latest News</h1>
+            <p>Stay tuned for the latest updates.</p>
+        </div>
+    );
+};
+
+export default News;
