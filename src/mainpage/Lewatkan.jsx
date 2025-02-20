e0b7298b5ce5bc0e39a0d3c250a7a39d02fc8da6
sxany <97228203+sxany@users.noreply.github.com>
2025-02-20 13:14:01 +0800

    change file location

---
+++ src/mainpage/Lewatkan.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../assets/css/local.css';
+import '../assets/css/input.css';
+
+const Lewatkan = () => {
+    return (
+        <div className='mx-auto -mt-2' style={{maxWidth:'1280px'}}>
+            {/*kotak*/}
+             <div className='relative z-10 flex space-x-2'>
+                <div className='kotak w-[34px] h-[13px] mt-4 '></div>
+                    <p className=' text-black mt-1.5 opacity-100 ' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '500', fontStyle: 'normal', fontSize:'20px'}}>Informasi Anda Lewatkan</p>
+                    </div>
+            {/*kotak informasi dan text */}
+            <div className='ml-12 flex relative space-x-2'>
+                <img src='./images/mumbai.jpg'className='w-[180px] h-auto'></img>
+                    <div className='flex flex-col'>
+                        <p className='ml-2  text-black opacity-100 max-w-[200px]' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mollis leo, ac commodo ipsum. Nunc at sapien quis justo euismod ornare. Aliquam augue nulla, laoreet ut urna quis,</p>
+                            <p className='ml-2  text-[#706969] opacity-100 ' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>12 jam lalu</p>
+                </div>
+                <img src='./images/mumbai.jpg'className='w-[180px] h-auto'></img>
+                    <div className='flex flex-col'>
+                        <p className='ml-2  text-black opacity-100 max-w-[200px]' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mollis leo, ac commodo ipsum. Nunc at sapien quis justo euismod ornare. Aliquam augue nulla, laoreet ut urna quis,</p>
+                            <p className='ml-2  text-[#706969] opacity-100 ' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>12 jam lalu</p>
+                
+                </div>
+                <img src='./images/mumbai.jpg'className='w-[180px] h-auto'></img>
+                    <div className='flex flex-col'>
+                        <p className='ml-2  text-black opacity-100 max-w-[200px]' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mollis leo, ac commodo ipsum. Nunc at sapien quis justo euismod ornare. Aliquam augue nulla, laoreet ut urna quis,</p>
+                            <p className='ml-2  text-[#706969] opacity-100 ' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>12 jam lalu</p>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Lewatkan;
