8d9bd834c06b4949cce24f4d1f04070986823fe5
sxany <97228203+sxany@users.noreply.github.com>
2025-02-20 13:11:37 +0800

    changed max-w and added aspect-auto on iframe

--- src/Musik.jsx
+++ src/Musik.jsx
@@ -11,9 +11,9 @@ const Gameplay = () => {
                     <p className=' text-black mt-1.5 opacity-100 ' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '500', fontStyle: 'normal', fontSize:'20px'}}>Musik Hari Ini</p>
                     </div>
                     {/*Youtube iframe*/}    
-                        <div>
-                            <iframe className='absolute ml-24 rounded-md' width="300" height="200" src="https://www.youtube.com/embed/?listType=playlist&list=RDdJf4wCdLU18&autoplay=0&mute=1"  title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullScreen></iframe>
-                        </div>  
+                            <div>
+                                <iframe className='relative ml-24 rounded-md h-[200px] max-w-[300px]aspect-auto' src="https://www.youtube.com/embed/?listType=playlist&list=RDdJf4wCdLU18&autoplay=0&mute=1"  title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullScreen></iframe>
+                            </div>  
                                               
         </div>
     );
