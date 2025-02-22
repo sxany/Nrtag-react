8be9f96a9c690fd47254c6db01c76374e28eda1e
sxany <97228203+sxany@users.noreply.github.com>
2025-02-22 14:23:10 +0800

    #

--- src/mainpage/Lewatkan.jsx
+++ src/mainpage/Lewatkan.jsx
@@ -4,7 +4,7 @@ import '../assets/css/input.css';
 
 const Lewatkan = () => {
     return (
-        <div className='mx-auto max-md:mt-[300px]' style={{maxWidth:'1280px'}}>
+        <div className='relative mx-auto max-md:mt-[500px]' style={{}}>
             {/*kotak*/}
              <div className='relative z-10 flex space-x-2'>
                 <div className='kotak w-[34px] h-[13px] mt-4 '></div>
@@ -23,6 +23,12 @@ const Lewatkan = () => {
                             <p className='md:ml-2  text-[#706969] opacity-100 ' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>12 jam lalu</p>
                 
                 </div>
+                <img src='./images/mumbai.jpg'className='md:w-[180px] md:max-h-[180px]'></img>
+                    <div className='min-md:flex md:flex-col max-md:flex max-md:flex-col'>
+                        <p className='md:ml-2  text-black opacity-100 md:max-w-[200px]' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mollis leo, ac commodo ipsum. Nunc at sapien quis justo euismod ornare. Aliquam augue nulla, laoreet ut urna quis,</p>
+                            <p className='md:ml-2  text-[#706969] opacity-100 ' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>12 jam lalu</p>
+                </div>
+
                 <img src='./images/mumbai.jpg'className='md:w-[180px] md:max-h-[180px]'></img>
                     <div className='min-md:flex md:flex-col max-md:flex max-md:flex-col'>
                         <p className='md:ml-2  text-black opacity-100 md:max-w-[200px]' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mollis leo, ac commodo ipsum. Nunc at sapien quis justo euismod ornare. Aliquam augue nulla, laoreet ut urna quis,</p>
