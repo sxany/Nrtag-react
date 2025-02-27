d828a82b88e47dcf7f5f394e57ab74414bc7baaa
sxany <97228203+sxany@users.noreply.github.com>
2025-02-27 22:52:59 +0800

    #

--- src/Populer.jsx
+++ src/Populer.jsx
@@ -13,7 +13,7 @@ const Populer = () => {
                         {/*Kotak Informasi dan text */}
                             <div className='max-md:grid max-md:grid-flow-row relative z-10 md:ml-24 max-md:ml-12 md:space-y-2 max-md:space-y-2'>
                             <div className='md:flex max-md:flex'>
-                                    <img src='./images/Unfinished.jpg'className='w-[180px] max-h-[180px] '
+                                    <img src='./images/carousel 2.jpg'className='w-[150px] max-h-[180px] '
                                     ></img>
                                     <div className=' space-y-1'>
                                     <p className='ml-2   text-black opacity-100 max-w-[200px]' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mollis leo, ac commodo ipsum. Nunc at sapien quis justo euismod ornare. Aliquam augue nulla, laoreet ut urna quis,</p>
@@ -22,7 +22,7 @@ const Populer = () => {
                                     </div>
 
                                 <div className='md:flex max-md:flex ml-0'>
-                                    <img src='./images/Unfinished.jpg'className='w-[180px] max-h-[180px]'
+                                    <img src='./images/carousel 2.jpg'className='w-[150px] max-h-[180px]'
                                     ></img>
                                     <div className=' space-y-1'>
                                     <p className='ml-2 text-black opacity-100 max-w-[200px]' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mollis leo, ac commodo ipsum. Nunc at sapien quis justo euismod ornare. Aliquam augue nulla, laoreet ut urna quis,</p>
@@ -31,7 +31,7 @@ const Populer = () => {
                                     </div>
 
                                     <div className='md:flex max-md:flex'>
-                                    <img src='./images/Unfinished.jpg'className='w-[180px] max-h-[180px]'
+                                    <img src='./images/carousel 2.jpg'className='w-[150px] '
                                     ></img>
                                     <div className='space-y-1'>
                                     <p className='ml-2 text-black opacity-100  max-w-[200px]' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mollis leo, ac commodo ipsum. Nunc at sapien quis justo euismod ornare. Aliquam augue nulla, laoreet ut urna quis,</p>
@@ -40,7 +40,7 @@ const Populer = () => {
                                 </div>
 
                                 <div className='md:flex max-md:flex'>
-                                    <img src='./images/Unfinished.jpg'className='w-[180px] max-h-[180px]'
+                                    <img src='./images/carousel 2.jpg'className='w-[150px] '
                                     ></img>
                                     <div className=' space-y-1'>
                                     <p className='ml-2 text-black opacity-100 flex max-w-[200px]' style={{fontFamily: "Familjen Grotesk, serif", fontWeight: '300', fontStyle: 'normal', fontSize:'11px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mollis leo, ac commodo ipsum. Nunc at sapien quis justo euismod ornare. Aliquam augue nulla, laoreet ut urna quis,</p>
